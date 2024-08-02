89b50f70236e5d960c7154b18e166cb1b27b1f00
RiahiYassine <yassineriahi0417@gmail.com>
2024-08-02 11:40:52 +0100

    new typeoperation : carburant

---
+++ src/main/java/au/gestionparcautomobile/aulsh/records/CarburantFilter.java
@@ -0,0 +1,13 @@
+package au.gestionparcautomobile.aulsh.records;
+
+public record CarburantFilter(
+
+        String immatriculation,
+        String typeImmatriculation,
+        String marque,
+        String modele,
+        String typeCarburant,
+        String centre
+
+) {
+}
